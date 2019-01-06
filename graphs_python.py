d7cdd6b93adff3b77286d567a4ccc555ff70e15c
unknown <scindia.harsh@gmail.com>
2019-01-06 17:23:17 +0530

    graphs.py

--- graphs_python.py
+++ graphs_python.py
@@ -1,1 +1,9 @@
 # graphs code from python-course.eu
+graph = {"a": ["c"],
+         "b": ["c", "e"],
+         "c": ["a","b","d","e"],
+         "d": ["c"],
+         "e": ["c","b"],
+         "f": []
+         }
+
