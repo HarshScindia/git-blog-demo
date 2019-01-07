230579fff440dede9f7c2c2ae5e5fab887dd252e
unknown <scindia.harsh@gmail.com>
2019-01-07 23:41:14 +0530

    added function add_edge

--- graphs_python.py
+++ graphs_python.py
@@ -51,3 +51,20 @@ class Graph(object):
             for neighbour in graph[node]:
                 edges.append((node, neighbour))
         return edges
+
+    def add_vertex(self, vertex):
+        """ If the vertex "vertex" is not in self.__graph_dict, a key  "vertex'
+        with an empty list as a value is added to the dictionary. Otherwise nothing
+        has to be done."""
+        if vertex not in self.__graph_dict:
+            self.__graph_dict[vertex] = []
+
+    def add_edge(self, edge):
+        """ assumes that edge is of type set, tuple or list; between two vertices 
+        can be multiple edges! """
+        edge = set(edge)
+        (vertex1, vertex2) = tuple(edge)
+        if vertex1 in self.__graph_dict:
+            self.__graph_dict[vertex1].append(vertex2)
+        else:
+            self.__graph_dict[vertex1] = [vertex2]
