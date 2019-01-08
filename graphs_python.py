c2a311f340ec660bb510ed03a720420cf8053c30
unknown <scindia.harsh@gmail.com>
2019-01-08 22:26:59 +0530

    graphs-python

--- graphs_python.py
+++ graphs_python.py
@@ -101,6 +101,17 @@ class Graph(object):
 
     def find_all_paths(self, start_vertex, end_vertex, path=[]):
         """ find all paths from start vertex to end_vertex in graph """
+        graph = self.__graph_dict
+        path = path + [start_vertex]
+        if start_vertex == end_vertex:
+            return [path]
+        path = []
+        for vertex in graph[start_vertex]:
+            if vertex not in path:
+                extended_paths = self.find_all_paths(vertex, end_vertex, path)
+                for p in extended_paths:
+                    paths.append(p)
+        return paths
 if __name__ == "__main__":
 
     g = { "a" : ["d"],
