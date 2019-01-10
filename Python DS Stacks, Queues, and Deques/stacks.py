ebaf2ac36eb35d4d540fe0360769972bb36b0cff
unknown <scindia.harsh@gmail.com>
2019-01-10 23:43:41 +0530

    Added docstring for push method

--- Python DS Stacks, Queues, and Deques/stacks.py
+++ Python DS Stacks, Queues, and Deques/stacks.py
@@ -3,6 +3,11 @@ class Stack:
         self.items = []
 
     def push(self, item):
+        """ Accepts an item as a parameter and appends it to the end of our list.
+            Returns None
+            Runtime: The runtime for this metyhod is O(1), or constant time, because
+            appending to the end of the list happpens in constant time.
+        """
         self.items.append(item)
 
     def pop(self):
@@ -16,3 +21,5 @@ class Stack:
 
     def is_empty(self):
         pass
+
+
