95ad323a1c0799ad2a9908deecbce523346d161f
unknown <scindia.harsh@gmail.com>
2019-01-11 00:27:27 +0530

    added docstring to queue function

--- Python DS Stacks, Queues, and Deques/queue.py
+++ Python DS Stacks, Queues, and Deques/queue.py
@@ -5,6 +5,13 @@ class Queue:
         self.items = []
 
     def enqueue(self, item):
+        """ Takes in an item and inserts that item into the 0th index
+            of the list that is representing the Queue.
+
+            Runtime: The runtime is O(n), or linear time because inserting into the 0th 
+            index of the list forces all the other items in the list to move one index to
+            the right 
+        """
         self.itmes.insert(0, item)
 
     def dequeu(self):
