254ebb146f1ede30d4453dfba1e0144f2047b52d
unknown <scindia.harsh@gmail.com>
2019-01-11 00:21:22 +0530

    added 'stubs for queue class

--- Python DS Stacks, Queues, and Deques/queue.py
+++ Python DS Stacks, Queues, and Deques/queue.py
@@ -1,1 +1,20 @@
 # Class for queue data structure
+
+class Queue:
+    def __init__(self):
+        self.item = []
+
+    def enqueue(self, items):
+        pass
+
+    def dequeu(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def size(self):
+        pass
+
+    def is_empty(self):
+        pass
