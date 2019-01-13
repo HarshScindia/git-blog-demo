b55292d96c21592b2704c999b75f0ab2cc9b28ef
unknown <scindia.harsh@gmail.com>
2019-01-13 13:18:36 +0530

    added docstrings for add_front and add_rear methods

--- Python DS Stacks, Queues, and Deques/deque.py
+++ Python DS Stacks, Queues, and Deques/deque.py
@@ -4,9 +4,19 @@ class Deque:
         self.items = []
 
     def add_front(self, item):
+        """ Takes an item as a parameter and inserts it into the zeroth index of the
+            list that is representing the deque 
+            Runtime: is linear or O(N), or linear, as all the other items in the
+            list need to shift one position to the right.
+        """
         self.items.insert(0, item)
 
     def add_rear(self, item):
+        """ Takes in an item as a parameter and appends that item to the end of the list
+            that is representing the deque.
+            The runtime is constant time because appending to the end of the list
+            happens in constant time 
+        """
         self.items.append(item)
 
     def remove_front(self):
