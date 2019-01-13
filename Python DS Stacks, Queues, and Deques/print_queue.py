9bcc93e89e9cdbfd545a6566d715da294820e204
unknown <scindia.harsh@gmail.com>
2019-01-13 16:01:34 +0530

    classs stub for printQueue

---
+++ Python DS Stacks, Queues, and Deques/print_queue.py
@@ -0,0 +1,31 @@
+# The Print Queue Challenge
+""" Create 3 classes that, together, model how a printer could take print jobs
+    out of aq print queue.
+    Requirements
+    PrintQueue class that follows the queue data structure implementation.
+    Job class pages attribute - random 1 to 10
+        print_page() - decrement pages
+        check_complete() - which will check wether all the pages are printed
+    Printer Class - 
+        get_job() - account for the case where
+        PrintQueue.items is empty
+        print_job() """
+
+class PrintQueue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, items):
+        pass
+
+    def deque(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def size(self):
+        pass
+
+    def is_empty(self):
+        pass
